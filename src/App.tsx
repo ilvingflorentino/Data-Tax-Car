2b3b338c7abe3f1a7e237dc9cbcda2b55dd5c513
ilving Florentino <ilvingflorentino2@gmail.com>
2025-02-17 13:50:02 -0400

    Listo 🧑‍💻 para desplegar 🚧 a Produccion.🎉

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import UsersPage from "./page/UsersPage";
 import VisitCounter from "./Components/VisitCounter";
-import { CalculatorFilled, CalculatorTwoTone } from "@ant-design/icons";
 
 export default function App() {
   return (
     <div style={{ padding: "9px" }}>
       <center>
         <h1>
-          <CalculatorTwoTone
-            style={{ color: "#1890ff", fontSize: "40px", marginRight: "8px" }}
-          />
           Calculadora de Importacion de Vehiculos de la Republica Dominicana.{" "}
-          {""}
-          <CalculatorFilled
-            style={{ color: "#1890ff", fontSize: "40px", marginRight: "8px" }}
-          />
         </h1>
       </center>
       <br></br>
@@ -23,10 +15,7 @@ export default function App() {
 
       <center style={{ marginTop: "10px" }}>
         <b>
-          By <img src="/k.png" alt="K3D Logo" style={{ height: "20px" }} />
-          <img src="/number-3.png" alt="K3D Logo" style={{ height: "20px" }} />
-          <img src="/d.png" alt="K3D Logo" style={{ height: "20px" }} />
-          Technology.
+          By <h4>K3D Technology</h4>
         </b>
       </center>
     </div>
